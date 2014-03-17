c7eb5b152739a73b854083dae38d5f83419ca163
Rupert Dance <rupertd@gmail.com>
2014-03-17 12:20:07 +1100

    Init concrete clean

---
+++ config/site.php
@@ -0,0 +1,6 @@
+<?php 
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'ConcreteClean');
+define('PASSWORD_SALT', 'DwNKsr3gE4wbivfeSBYe8VPsEWVsrZkfgroQ3MA84Mzqu0BehcugrTz4bnRQQoEn');
